ac063d4db8000f8d4a45646f762125851015265b
Ryan <ryanwesterberg@gmail.com>
2017-11-26 12:06:09 -0600

    new reel

--- bgvideo-v2.js
+++ bgvideo-v2.js
@@ -1,5 +1,5 @@
 document.body.innerHTML += `
-<video autoplay loop>
+<video autoplay loop muted>
     <source src="https://cdn.rawgit.com/rwest202/fred-edits-bgvideo/master/Into_the_Wild.webm">
     <source src="https://cdn.rawgit.com/rwest202/fred-edits-bgvideo/master/Into_the_Wild.mp4">
 </video>
