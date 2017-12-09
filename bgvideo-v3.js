b64b19cc8de5a6c1b61c5107093b96f4e5c086ee
Ryan <ryanwesterberg@gmail.com>
2017-12-09 14:05:49 -0600

    home fix

--- bgvideo-v3.js
+++ bgvideo-v3.js
@@ -1,6 +1,9 @@
-document.body.innerHTML += `
-<video autoplay loop muted playsinline>
-    <source src="https://cdn.rawgit.com/rwest202/fred-edits-bgvideo/master/edit_reel.webm">
-    <source src="https://cdn.rawgit.com/rwest202/fred-edits-bgvideo/master/edit_reel.mp4">
-</video>
-`
+let path = window.location.pathname
+
+if (path === '/') {
+    document.body.innerHTML += `
+    <video autoplay loop muted playsinline>
+        <source src="https://cdn.rawgit.com/rwest202/fred-edits-bgvideo/master/edit_reel.webm">
+        <source src="https://cdn.rawgit.com/rwest202/fred-edits-bgvideo/master/edit_reel.mp4">
+    </video>
+}
