aea25558790ddcee5a920c6ebd97f6ad3cbe7742
Ryan <ryanwesterberg@gmail.com>
2018-01-10 19:51:48 -0600

    Update bgvideo-v5.js

--- bgvideo-v5.js
+++ bgvideo-v5.js
@@ -1,7 +1,7 @@
 let path = window.location.pathname
 
 if (path === '/') {
-    document.body.innerHTML += `<video autoplay loop muted plays-inline>
+    document.body.innerHTML += `<video autoplay loop muted playsinline>
         <source src="https://cdn.rawgit.com/rwest202/fred-edits-bgvideo/master/edit_reel.webm">
         <source src="https://cdn.rawgit.com/rwest202/fred-edits-bgvideo/master/edit_reel.mp4">
     </video>`
