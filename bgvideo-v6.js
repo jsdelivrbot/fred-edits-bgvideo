fd1ff72b7a869c5b3e64647de5af5663050bd61d
Ryan <ryanwesterberg@gmail.com>
2018-01-10 19:54:37 -0600

    Update and rename bgvideo-v5.js to bgvideo-v6.js

--- bgvideo-v6.js
+++ bgvideo-v6.js
@@ -1,7 +1,7 @@
 let path = window.location.pathname
 
 if (path === '/') {
-    document.body.innerHTML += `<video autoplay loop muted playsinline>
+    document.body.innerHTML += `<video playsinline loop muted autoplay>
         <source src="https://cdn.rawgit.com/rwest202/fred-edits-bgvideo/master/edit_reel.webm">
         <source src="https://cdn.rawgit.com/rwest202/fred-edits-bgvideo/master/edit_reel.mp4">
     </video>`
